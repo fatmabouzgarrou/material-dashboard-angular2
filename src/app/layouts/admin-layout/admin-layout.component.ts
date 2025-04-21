a7fe9f68b070f35403976fb3e7a829e4ca7ca204
fatma bouzgarrou <fatmabouzgarrou6@gmail.com>
2025-04-21 21:01:44 +0100

    third commit

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,17 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-layout',
-  template: `
-    
-    <div>
-      <router-outlet></router-outlet>
-    </div>
-  `,
+  templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss']
 })
-export class AdminLayoutComponent implements OnInit {
-  ngOnInit() {
-    console.log('AdminLayoutComponent loaded');
-  }
+export class AdminLayoutComponent {
+  constructor() {}
 }
